57b0e21129490f3762d90873c895bcd714c6e369
dugku <iphonerespon@gmail.com>
2023-09-01 18:25:22 -0500

    Here we go

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
+from sqlalchemy import update
 import random
 
 import models
@@ -38,7 +38,7 @@ async def input_Todo(todo: Todo, db: Session = Depends(get_db)):
     db.add(taskEntry)
     db.commit()
     return {'message': "Completed"} 
-@app.get("/todo/{task}", )
+@app.get("/todo/{task}", )   
 async def get_todo(task:str ):
     if task in itemDict:
         return {"task": task, **itemDict[task]}
@@ -47,8 +47,21 @@ async def get_todo(task:str ):
 
 
 @app.put("/complete/{task_id}")
-async def complete_todo(task_id: int, db: Session = Depends(get_db)):
-    taks_obj = db.query(models.TaskQue).filter(models.TaskQue.task_id == task_id).first()
-    
-    """setattr(taks_obj, "True", models.TaskQue.complete)"""
-    return {"Does it work?": taks_obj}
+async def complete_todo(task_id: int,todo: Todo ,db: Session = Depends(get_db)):
+    change_complete = db.query(models.TaskQue).filter(models.TaskQue.task_id == task_id).update({"complete": True})
+
+    if change_complete == None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    db.commit()
+    return {"Does it work?": "Maybe"}
+
+@app.delete('/delete/{task_id}')
+async def delete_tas(task_id: int,db: Session = Depends(get_db)):
+    change_complete = db.query(models.TaskQue).filter(models.TaskQue.task_id == task_id).delete()
+
+    if change_complete == None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    db.commit()
+    return{"message":"Deleted"}
